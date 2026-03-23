cc19db2a49a95eec1b1264692b9c65db8b87e10c
choimi2005 <maxchoi88@gmail.com>
2026-03-23 21:58:44 +0900

    first commit

---
+++ hook/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+
+import './App.css'
+import Dog from './compoents/Dog'
+import Garden from './compoents/Garden'
+import Counter from './compoents/Count'
+import Car from './compoents/Car'
+import AddProduct from './compoents/AddProduct'
+import Box from './compoents/Box'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+    <h2>state 상태관리</h2>
+    {/* <Box></Box>박스사이의 모든것이 {children}으로*/}
+    <Box>
+      <h3>박스 안의 내용</h3>
+      <p>이것은 box 의 내용물 입니다</p>
+    </Box>
+
+    <Box>
+      <h3>또 다른 박스안의 내용</h3>
+      <p>다른 box 의 내용물 입니다</p>
+    </Box>
+
+    {/* <AddProduct/> */}
+
+
+    {/* <Car></Car> */}
+
+    {/* <Counter/> */}
+    {/* <Dog breed = "말티즈" age = {3} />
+     <Dog breed = "진돗개" age = {5} /> */}
+    
+    {/* <Garden></Garden> */}
+    
+    </>
+  )
+}
+
+export default App
